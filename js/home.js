043af6d367189beb0e9e11424c23a0df9ee8b0cc
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2024-12-08 22:10:12 +0600

    toggling between donation and history added

--- js/home.js
+++ js/home.js
@@ -16,9 +16,9 @@ document.getElementById('donate-btn-noakhali').addEventListener('click', functio
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     newDiv.innerHTML = `
-        <div class="card bg-base-100 w-full shadow-xl">
+        <div class="card bg-base-100 border border-gray-300 w-3/4 mx-auto">
             <div class="card-body">
-               <p> ${donateAmount} Taka is Donated for Flood-2024 at Feni, Bangladesh</p>
+               <p class="font-bold"> ${donateAmount} Taka is Donated for Flood-2024 at Noakhali, Bangladesh</p>
                <p> Date: ${dateTimeString} (${timeZone}) </p>
             </div>
         </div>
@@ -27,11 +27,42 @@ document.getElementById('donate-btn-noakhali').addEventListener('click', functio
 
 })
 
-document.getElementById('donate-btn-noakhali').addEventListener('click', function () {
-    const donateAmount = getDonateAmountById('donate-amount-noakhali');
-    const existingAmount = getExistingAmountById('total-amount-noakhali');
+document.getElementById('donate-btn-feni').addEventListener('click', function () {
+    const donateAmount = getDonateAmountById('donate-amount-feni');
+    const existingAmount = getExistingAmountById('total-amount-feni');
     const totalAmount = donateAmount + existingAmount;
-    document.getElementById('total-amount-noakhali').innerText = totalAmount;
+    document.getElementById('total-amount-feni').innerText = totalAmount;
+
+    const totalMoneyOnAccount = getTotalMoneyOnAccountById('total-money-on-account');
+    const totalMoneyOnAccountAfterDonation = totalMoneyOnAccount - donateAmount;
+    document.getElementById('total-money-on-account').innerText = totalMoneyOnAccountAfterDonation;
+
+    const transactionHistoryPage = document.getElementById('transaction-history-page');
+    let newDiv = document.createElement('div');
+    
+
+    let currentDateTime = new Date();
+    let dateTimeString = currentDateTime.toLocaleString();
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    newDiv.innerHTML = `
+        <div class="card bg-base-100 border border-gray-300 w-3/4 mx-auto">
+            <div class="card-body">
+               <p class="font-bold"> ${donateAmount} Taka is Donated for Flood-2024 at Noakhali, Bangladesh</p>
+               <p> Date: ${dateTimeString} (${timeZone}) </p>
+            </div>
+        </div>
+    `
+    transactionHistoryPage.appendChild(newDiv);
+
+})
+
+
+document.getElementById('donate-btn-quota-movement').addEventListener('click', function () {
+    const donateAmount = getDonateAmountById('donate-amount-quota-movement');
+    const existingAmount = getExistingAmountById('total-amount-quota-movement');
+    const totalAmount = donateAmount + existingAmount;
+    document.getElementById('total-amount-quota-movement').innerText = totalAmount;
 
     const totalMoneyOnAccount = getTotalMoneyOnAccountById('total-money-on-account');
     const totalMoneyOnAccountAfterDonation = totalMoneyOnAccount - donateAmount;
@@ -45,9 +76,9 @@ document.getElementById('donate-btn-noakhali').addEventListener('click', functio
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     newDiv.innerHTML = `
-        <div class="card bg-base-100 w-full shadow-xl">
+        <div class="card bg-base-100 border border-gray-300 w-3/4 mx-auto">
             <div class="card-body">
-               <p> ${donateAmount} Taka is Donated for Flood-2024 at Feni, Bangladesh</p>
+               <p class="font-bold"> ${donateAmount} Taka is Donated for Flood-2024 at Noakhali, Bangladesh</p>
                <p> Date: ${dateTimeString} (${timeZone}) </p>
             </div>
         </div>
@@ -58,6 +89,18 @@ document.getElementById('donate-btn-noakhali').addEventListener('click', functio
 
 
 
-document.getElementById('history-btn').addEventListener('click', function () {
 
+
+document.getElementById('history-btn').addEventListener('click', function () {
+    const transactionHistoryPage = document.getElementById("transaction-history-page");
+    transactionHistoryPage.classList.remove('hidden');
+    const cardsSection = document.getElementById('cards-section');
+    cardsSection.classList.add('hidden');
+})
+
+document.getElementById("donation-btn").addEventListener('click', function(){
+    const transactionHistoryPage = document.getElementById("transaction-history-page");
+    transactionHistoryPage.classList.add('hidden');
+    const cardsSection = document.getElementById('cards-section');
+    cardsSection.classList.remove('hidden');
 })
